a9fd56f97ecb856a743f0080f3afa9a877bb8f4d
mehedi <darkhasan10@gmail.com>
2024-07-18 21:49:48 +0530

    poject setup done

---
+++ README.md
@@ -0,0 +1,4 @@
+# Todo app
+
+This Todo app provides you to setup your daily task.
+
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+
+function App() {
+
+  return (
+    <div className="main w-full h-screen bg-gray-800"></div>
+  )
+}
+
+export default App
