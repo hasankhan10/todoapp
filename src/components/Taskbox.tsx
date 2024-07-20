4b2a3b4bafe43a093a680bbf2fa1b1e78cb5ab54
mehedi <darkhasan10@gmail.com>
2024-07-20 22:46:14 +0530

    create delete functionality from tasklist

--- src/components/Taskbox.tsx
+++ src/components/Taskbox.tsx
@@ -1,10 +1,10 @@
 import Tasklist from "./Tasklist"
 import { useState } from "react";
 
-const items:string[] = []
-
 function Taskbox() {
     const [value,setValue] = useState("")
+    const [items, setItems] = useState<string[]>([]);
+
 
     const getValue = (e:React.ChangeEvent<HTMLInputElement>)=>{
         const {value} = e.target;
@@ -16,11 +16,19 @@ function Taskbox() {
             alert("Please add some task")
             return
         }
-        items.push(value)
+        setItems([...items.reverse(),value])
         setValue("")
     }
+    const deleteTask = (index:number)=>{
+        const updatedItem:string[] = items.filter((_,i)=> i!==index)
+        setItems(updatedItem)
+    }
+    const editTask = (index:number)=>{
+        console.log(index);
+        
+    }
   return (
-    <div className="taskbox w-[550px] min-h-[400px] p-2 bg-slate-400 rounded-md">
+    <div className="taskbox w-[550px] min-h-[100px] p-2 bg-slate-400 rounded-md">
         <section className="addtask w-full h-10 flex justify-center gap-3">
             <input className=" w-[80%] h-full text-center rounded-md" type="text" placeholder="Add task" onChange={ getValue} value={value} name="addtask"/>
             <button onClick={addTask} className=" px-5 py-2 bg-blue-700 rounded-md hover:scale-105 transition-all">Add</button>
@@ -29,7 +37,7 @@ function Taskbox() {
             {
                 items && items.map((item:string,index:number) => (
 
-                    <Tasklist key={index} task = {item} />
+                    <Tasklist key={index} task = {item} onDelete={()=>deleteTask(index)} onEdit={()=>editTask(index)} />
                 ))
             }
         </section>
--- src/components/Tasklist.tsx
+++ src/components/Tasklist.tsx
@@ -2,22 +2,24 @@ import { useState } from "react";
 import { MdModeEdit } from "react-icons/md";
 import { MdDelete } from "react-icons/md";
 
-type Task = {
-    task: string
+type TaskProps = {
+    task: string,
+    onDelete:()=>void,
+    onEdit:()=>void
 }
 
-function Tasklist({task}:Task) {
+function Tasklist({task,onDelete,onEdit}:TaskProps) {
     const [check,setCheck] = useState(true)
-    console.log(check);
-    
   return (
     
     <div className="tasklist w-[90%] flex py-3 bg-slate-300 mt-2 px-2 gap-2 mx-auto rounded-md">
         <input className=" cursor-pointer w-4" type="checkbox" name="box" id="" onChange={()=> setCheck(!check)} />
-        <p>{task}</p>
+        {
+          check ? <p>{task}</p>:<s className=" opacity-60">{task}</s>
+        }
         <div className="iconbox flex ml-auto gap-4">
-            <MdModeEdit className=" w-5 h-5 cursor-pointer" />
-            <MdDelete className=" w-5 h-5 cursor-pointer" />
+            <MdModeEdit onClick={onEdit} className=" w-5 h-5 cursor-pointer" />
+            <MdDelete onClick={onDelete} className=" w-5 h-5 cursor-pointer" />
         </div>
     </div>
   )
